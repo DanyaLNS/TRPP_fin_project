dedff470fba13749dda86b080da6443c83833bc8
DanyaLNS <danil-anikin-98@mail.ru>
2022-05-19 09:51:13 +0300

    Добавлен setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="TrppFinProject",
+    version="1.0",
+    author="Anikin Danila",
+    author_email="danil-anikin-98@mail.ru",
+    description="Итоговый проект по ТРПП студентов ИНБО-07-20",
+    packages = find_packages()
+)
